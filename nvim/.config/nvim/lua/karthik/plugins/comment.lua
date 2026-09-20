7fc4bac387af3ae434294eb642277f85ec03e997
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2026-09-20 17:11:39 +0530

    nvim config broken

--- nvim/.config/nvim/lua/karthik/plugins/comment.lua
+++ nvim/.config/nvim/lua/karthik/plugins/comment.lua
@@ -5,6 +5,11 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
+    vim.g.skip_ts_context_commentstring_module = true
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
+
     -- import comment plugin safely
     local comment = require("Comment")
 
